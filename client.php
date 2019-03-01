584a2d8e25a922bac651b69f2afb7b4dda2f662d
kirinstudio <44038082+kirinstudio@users.noreply.github.com>
2019-03-02 04:06:42 +0700

    Add files via upload

---
+++ client.php
@@ -0,0 +1,12 @@
+<?php
+$body = array('locale' => 'vi_VN',
+            'task_extra_info' => '',
+	        'task_name' => 'vip_watch_video_ball_01',
+	        'timezone' => 'GMT+07:00');
+	        
+	        
+	        
+#	        "locale":"vi_VN",
+#	"task_extra_info":"",
+#	"task_name":"vip_watch_video_ball_01",
+#	"timezone":"GMT+07:00"
