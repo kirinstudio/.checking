c5135b3ac7d20d0bc1c653b92056ccdd7ff169b5
kirinstudio <44038082+kirinstudio@users.noreply.github.com>
2019-03-11 06:54:31 +0700

    Add files via upload

---
+++ banner.php
@@ -0,0 +1,8 @@
+$lg =" 
+$red    _  __     _     
+$red   | |/ /_ __| |_   $kuning Created KrtbyNick
+$red   | ' /| '__| __|  $kuning Library$red PHP CLI
+$red   | . \| |  | |_   $kuning Compilation$ijo 9/3/2019
+$red   |_|\_\_|   \__|  $kuning YouTuber$ijo KirinStudio
+ $c \n\n";
+$banner ="\033[1;41m This is a trial version developed by Krt developer, please contribute the version and comment information, and send it to krtbynick@gmail.com email box thanks! $c\n\n\033[1;47m";
