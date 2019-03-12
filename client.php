16ef5797f0e45ac429ebc321442d9959557599b4
kirinstudio <44038082+kirinstudio@users.noreply.github.com>
2019-03-12 18:03:11 +0700

    Update client.php

--- client.php
+++ client.php
@@ -3,7 +3,7 @@ $body = array('locale' => 'vi_VN',
             'task_extra_info' => '',
 	        'task_name' => 'vip_watch_video_ball_01',
 	        'timezone' => 'GMT+07:00');
-	        
+	         $video = json_encode($body,true);
 	        
 	        
 #	        "locale":"vi_VN",
